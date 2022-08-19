2f4ff36428227253baa0e0db6d95532faabcbe76
Tatiwel <49209072+Tatiwel@users.noreply.github.com>
2022-08-18 22:19:37 -0300

    Configuração inicial para a criaçao de telas usando o Scene builder

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+
     }
+
+    @Override
+    public void start(Stage pprimaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        //titulo da janela
+        pprimaryStage.setTitle("Tela de Login");
+        // resto dos launchs
+        pprimaryStage.setScene(tela);
+        pprimaryStage.show();
+    }
+
 }
