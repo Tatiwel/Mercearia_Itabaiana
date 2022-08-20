c633ac0ae59fdf81bec72f101d86779087c10d8d
Tatiwel <49209072+Tatiwel@users.noreply.github.com>
2022-08-20 13:41:18 -0300

    Criação da Classe Home, e Atualização do Código
    Criação da classe Home para receber os métodos referente a tela principal. Adição de dois Boetoes ( Cadastro de Produtos e Cadastro de Categorias)
    Atualização no código corrigindo erros de digitação.

--- src/App.java
+++ src/App.java
@@ -4,23 +4,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application{
+public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
 
     }
 
     @Override
-    public void start(Stage pprimaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
         Parent root = fxmlLoader.load();
         Scene tela = new Scene(root);
 
-        //titulo da janela
-        pprimaryStage.setTitle("Tela de Login");
+        // titulo da janela
+        primaryStage.setTitle("Sistema de Gestão da Mercearia");
         // resto dos launchs
-        pprimaryStage.setScene(tela);
-        pprimaryStage.show();
+        primaryStage.setScene(tela);
+        primaryStage.show();
     }
 
 }
---
+++ src/Home.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Home {
+
+    @FXML
+    private Button btCadastrarCategorias;
+
+    @FXML
+    private Button btCadastrarProdutos;
+
+    @FXML
+    void clickCategoria(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickProduto(ActionEvent event) {
+
+    }
+
+}
